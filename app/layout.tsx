d440c5f09f2a25eec28cb8863878ef394e98035c
callmeniyu <getniyashere@gmail.com>
2025-01-01 10:47:40 +0530

    remove toaster from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
         className={workSans.variable}
       >
         {children}
-        <Toaster />
+        {/* <Toaster /> */}
       </body>
     </html>
   );
